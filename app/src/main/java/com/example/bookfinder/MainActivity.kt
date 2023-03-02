d01ebd525bfe8bf065d27fbd21914f90822d5af5
Mikhail But <mikhail_but@outlook.com>
2023-03-02 12:32:48 +0600

    Icon

--- app/src/main/java/com/example/bookfinder/MainActivity.kt
+++ app/src/main/java/com/example/bookfinder/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.bookfinder
 
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,7 +9,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import com.example.bookfinder.ui.BookApp
 import com.example.bookfinder.ui.theme.BookFinderTheme
 
